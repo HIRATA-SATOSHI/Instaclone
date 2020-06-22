e4f0976e79c17592f308fea72b0997c6e708758e
satoshi.hirata <s.hirata0507@gmail.com>
2020-06-22 22:47:58 +0900

    user登録に写真投稿機能 #2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :image)
+      params.require(:user).permit(:name, :email, :password_digest, :image, :image_cache)
     end
 end

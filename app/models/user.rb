d4011d7aaca4c5bd863fa7aa75711c6f3238135c
satoshi.hirata <s.hirata0507@gmail.com>
2020-06-24 00:10:52 +0900

    PWエラー　_form.html.erbの31行目修正 #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
 
-  validates :password, presence: true, length:{minimum:6}
   has_secure_password
-
+  validates :password, presence: true, length:{minimum:6}
+  
   mount_uploader :image, ImageUploader
 end

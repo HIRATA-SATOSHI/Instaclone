d7e7392a63fec7657636e4adc7eb8732d61fd184
satoshi.hirata <s.hirata0507@gmail.com>
2020-07-02 00:17:44 +0900

    gem 'letter_opener install #6

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :pictures
   resources :favorites, only: [:create, :destroy, :index]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
